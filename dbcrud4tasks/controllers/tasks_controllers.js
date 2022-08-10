12f872ac6bdd58717d0f75fb55a6d77ec0ef9d83
Serhii <lotsman.961@gmail.com>
2022-08-10 21:37:58 +0300

    add commits

--- dbcrud4tasks/controllers/tasks_controllers.js
+++ dbcrud4tasks/controllers/tasks_controllers.js
@@ -4,23 +4,28 @@ export function getTasks() {
     return pool.query('SELECT * FROM items')
         .then(res => res.rows)
 }
+
 export function getSingleTask(id) {
     return pool.query('SELECT * FROM items WHERE id = $1', [id])
         .then(res => res.rows[0])
 }
-export function createTask(id, done, title, due_date) {
-    return pool.query('INSERT INTO public.items(id, done, title, due_date) VALUES ($1, $2, $3, $4)', [id, done, title, due_date])
+
+export function createTask(done, title, due_date) {
+    return pool.query('INSERT INTO public.items(done, title, due_date) VALUES ($1, $2, $3) RETURNING id, done, title, due_date', [done, title, due_date])
         .then(res => res.rows[0])
 }
+
 export function updateTask(id, done, title, due_date) {
-    return pool.query('UPDATE public.items SET done = $2, title = $3, due_date = $4 WHERE id = $1', [id, done, title, due_date])
+    return pool.query('UPDATE public.items SET done = $2, title = $3, due_date = $4 WHERE id = $1 RETURNING id, done, title, due_date', [id, done, title, due_date])
         .then(res => res.rows[0])
 }
-export function putTask(id, done, title) {
-    return pool.query('UPDATE public.items SET done = $2, title = $3, due_date = $4 WHERE id = $1', [id, done, title])
+
+export function putTask(id, done, title, due_date) {
+    return pool.query('UPDATE public.items SET done = $2, title = $3, due_date = $4 WHERE id = $1 RETURNING id, done, title, due_date', [id, done, title, due_date])
         .then(res => res.rows[0])
 }
+
 export function deleteTask(id) {
-    return pool.query('DELETE FROM public.items WHERE id = $1', [id])
+    return pool.query('DELETE FROM public.items WHERE id = $1 RETURNING id, done, title, due_date', [id])
         .then(res => res.rows[0])
 }
--- dbcrud4tasks/routes/tasks_routes.js
+++ dbcrud4tasks/routes/tasks_routes.js
@@ -12,36 +12,36 @@ router.patch('/tasks/:id', update);
 router.put('/tasks/:id', put);
 router.delete('/tasks/:id', del);
 
-function create(req, res) {
-    const {title, done} = req.body;
-    createTask(title, done).then(t => res.status(201).json(t))
+function create(req, res) { // curl localhost:4000/tasks -d '{"title": "created", "done": true, "due_date": "2022-10-10"}' -H "Content-Type: application/json"
+    const {done, title, due_date} = req.body;
+    createTask(done, title, due_date).then(t => res.status(201).json(t))
         .catch(() => res.sendStatus(500))
 }
-function get(req, res) {
+function get(req, res) { // curl localhost:4000/tasks
     getTasks().then(t => res.json(t))
         .catch(() => res.sendStatus(500))
 }
-function getSingle(req, res) {
+function getSingle(req, res) { // curl localhost:4000/tasks/2
     const id = req.params.id;
     getSingleTask(id).then(t => t ? res.json(t) : res.sendStatus(404))
         .catch(() => res.sendStatus(500))
 }
-function update(req, res) {
+function update(req, res) { // curl -X PATCH localhost:4000/tasks/2 -d '{"title": "Changed" }' -H "Content-Type: application/json"
     const id = req.params.id;
     getSingleTask(id).then(oldTask => Object.assign(oldTask, req.body))
         .then(({id, done, title, due_date}) => updateTask(id, done, title, due_date))
         .then(t => res.json(t))
        .catch(() => res.sendStatus(500))
 }
-function put(req, res) {
+function put(req, res) { // curl -X PUT localhost:4000/tasks/1 -d '{"title": "Changed", "due_date": "2022-05-05" }' -H "Content-Type: application/json"
     const id = req.params.id;
-    const done = req.body.done;
-    const title = req.body.title;
-    const due_date = req.body.dataset;
+    const done = req.body.done || false;
+    const title = req.body.title || 'text';
+    const due_date = req.body.due_date || '2022-01-01'
     putTask(id, done, title, due_date).then(t => res.json(t))
         .catch(() => res.sendStatus(500))
 }
-function del(req, res) {
+function del(req, res) { // curl -X DELETE localhost:4000/tasks/3
     const id = req.params.id;
     deleteTask(id).then(t => res.json(t))
         .catch(() => res.sendStatus(500))
