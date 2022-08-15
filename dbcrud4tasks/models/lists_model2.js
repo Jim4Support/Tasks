2b2afa4638cf5a18c1dcc5cd075d899dd4c38876
Serhii <lotsman.961@gmail.com>
2022-08-16 00:35:27 +0300

    SQL correct

--- dbcrud4tasks/models/lists_model2.js
+++ dbcrud4tasks/models/lists_model2.js
@@ -1,32 +1,20 @@
-import {knx} from "../knex.js";
+import {pool} from '../tasks_db.js'; // SQL
 
 export function getTodayCount() {
     const today = new Date();
-    return knx('items').count('title')
-        .whereBetween('due_date',[today, today])
+    return pool.query('SELECT COUNT(title) FROM items WHERE due_date BETWEEN $1 AND $2', [today, today])
+        .then(res => res.rows[0])
 }
 export function notDoneTasks() {
-    return knx.select('list.name', 'list.id')
-        .count('items.title as undone')
-        .where('items.done', false)
-        .orWhere('items.done', null)
-        .from('items')
-        .rightJoin('list', 'list.id', 'items.list_id')
-        .groupBy('list.name', 'list.id')
+    return pool.query('SELECT l.name AS name, i.list_id AS id, COUNT(i.done = false OR null) AS undone FROM items AS i RIGHT JOIN list AS l ON l.id = i.list_id GROUP BY l.name, i.list_id')
+        .then(res => res.rows)
 }
 export function getTodayTasks() {
     const today = new Date();
-    return knx.select('items.title as title', 'items.id as item_id', 'items.done', 'items.due_date', 'list.name as list', 'list.id as list_id')
-        .whereBetween('items.due_date', [today, today])
-        .from('items')
-        .leftJoin('list','list.id', 'items.list_id')
+    return pool.query('SELECT list.name AS lists, list.id AS list_id, done AS done, items.id AS items_id, due_date AS due_date, title FROM items LEFT JOIN list on list.id = items.list_id WHERE items.due_date BETWEEN $1 AND $2', [today, today])
+        .then(res => res.rows)
 }
 export function listUndoneTasks(listId, all) {
-    let query = knx.select('*')
-        .from('items')
-        .where('list_id', listId)
-    if (!all){
-        query=query.andWhere('done', false)
-    }
-    return query
+    return pool.query('SELECT * FROM items WHERE list_id = $1 AND done = false OR list_id = $1 AND done = $2',[listId, all])
+        .then(res => res.rows)
 }
--- dbcrud4tasks/routes/lists_routes2.js
+++ dbcrud4tasks/routes/lists_routes2.js
@@ -1,3 +1,4 @@
+// SQL
 import {getTodayCount, getTodayTasks, notDoneTasks, listUndoneTasks} from "../models/lists_model2.js";
 import Router from 'express';
 export const routerList = new Router();
@@ -8,11 +9,11 @@ routerList.get('/lists/:listId/tasks', undoneTasks);
 
 function getToday(req, res, next) { // curl localhost:4000/dashboard
     Promise.all([getTodayCount(), notDoneTasks()])
-        .then(t => res.json({'Today count': +t[0][0].count, 'Name list': t[1].map((el)=>{
-            return {name: el.name,
-                id: el.id,
-                undone: parseInt(el.undone)
-            }
+        .then(t => res.json({'Today count': +t[0].count, 'Name list': t[1].map((el)=>{
+                return {name: el.name,
+                    id: el.id,
+                    undone: parseInt(el.undone)
+                }
             })}))
         .catch(next)
 }
@@ -20,10 +21,10 @@ function todayTasks(req, res, next) { // curl localhost:4000/collections/today
     getTodayTasks().then(t => res.json(t.map(el => {
         return {
             title: el.title,
-            item_id: el.item_id,
+            item_id: el.items_id,
             done: el.done,
             date: el.due_date,
-            list: {list_name: el.list, list_id: el.list_id}
+            list: {list_name: el.lists, list_id: el.list_id}
         }
     })))
         .catch(next)
