9d1b175b7e1862362cf3b9c322c2eb4780ac7738
Serhii <88855803+Jim4Support@users.noreply.github.com>
2022-08-26 18:02:39 +0300

    Update toDoList.js

--- jsHTML/toDoList.js
+++ jsHTML/toDoList.js
@@ -1,18 +1,56 @@
 let data = [
-    {id: 1, dueDate: '23.08.22', done: false, name: 'hometask', description: 'make homework'},
-    {id: 2, dueDate: '24.08.22', done: false, name: 'officetask', description: 'make office work'},
-    {id: 3, dueDate: '22.08.22', done: false, name: 'shopping', description: 'buy vegetables'},
-    {id: 4, dueDate: '23.08.22', done: false, name: 'walk', description: 'go for a walk'},
-    {id: 5, dueDate: '21.08.22', done: false, name: 'CSS Figma', description: 'to do adaptive mobile first'}
+    {id: 1, dueDate: new Date('2022-08-26'), done: false, name: 'hometask', description: ''},
+    {id: 2, dueDate: new Date('2022-08-27'), done: false, name: 'officetask', description: 'make office work'},
+    {id: 3, dueDate: new Date('2022-08-26'), done: true, name: 'shopping', description: 'buy vegetables'},
+    {id: 4, dueDate: new Date(''), done: true, name: 'walk', description: 'go for a walk'},
+    {id: 5, dueDate: new Date('2022-08-25'), done: false, name: 'CSS Figma', description: 'to do adaptive mobile first'}
 ];
-const getList = document.getElementById('toDoList')
-function allTasks(task) {
-    const {dueDate, done, name, description} = task;
-    getList.innerHTML += `<div><p id="date">${dueDate}</p> <div id="name">${done} <strong>${name}</strong></div> <span>${description}</span></div>`
-}
-data.forEach(allTasks);
 
-function findById(id) {
-    let findId = data.find(f => f.id === id)
-    findId = findId.done !== true;
+const tasks = document.querySelector('div');
+
+function overdue(dueDate) {
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth() + 1;
+    const year = now.getFullYear();
+    const fullDate = `${day}.${month}.${year}`;
+    console.log(fullDate);
+    return now < dueDate ? true : false;
 }
+console.log(overdue(new Date('2022-08-25')));
+
+
+class isCheck {
+    constructor(check) {
+        if (check.checked) {
+            this.done = check.checked;
+        } else {
+            this.done = check.checked;
+        }
+    }
+}
+
+const generateTasks = (id, dueDate, done, name, description) => {
+    const day = new Date(dueDate).getDate();
+    const month = new Date(dueDate).getMonth() + 1;
+    const year = new Date(dueDate).getFullYear();
+    const fullDate = `${day}.${month}.${year}`
+    return `
+    <div class="todo-item">
+        <div class="date">
+        ${fullDate}
+        </div>
+        <div class="checkAndName">
+    <label><input type="checkbox" name="checkfield" id="isDone" ${done ? 'checked' : ''}><strong>${name}</strong></label>
+    </div>
+    <p class="desc">${description}<hr></p>
+    </div>`
+}
+
+const taskHTML = data.map(task => {
+    return generateTasks(task.id, task.dueDate, task.done, task.name, task.description);
+}).join('');
+
+tasks.innerHTML = taskHTML;
+
+console.log(tasks);
