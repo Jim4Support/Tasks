03679976898e10d00eb56f2483c81545e224d0d0
Serhii Lotsman <lotsman.961@gmail.com>
2022-08-27 20:58:46 +0300

    Output todo & control todo

--- jsHTML/toDoList.js
+++ jsHTML/toDoList.js
@@ -1,56 +1,91 @@
-let data = [
-    {id: 1, dueDate: new Date('2022-08-26'), done: false, name: 'hometask', description: ''},
-    {id: 2, dueDate: new Date('2022-08-27'), done: false, name: 'officetask', description: 'make office work'},
-    {id: 3, dueDate: new Date('2022-08-26'), done: true, name: 'shopping', description: 'buy vegetables'},
-    {id: 4, dueDate: new Date(''), done: true, name: 'walk', description: 'go for a walk'},
-    {id: 5, dueDate: new Date('2022-08-25'), done: false, name: 'CSS Figma', description: 'to do adaptive mobile first'}
-];
+const tasks = document.getElementById('tasks');
+const showTasksButtonElement = document.getElementById('showTasks');
+let switcher = false;
 
-const tasks = document.querySelector('div');
+showTasksButtonElement.onclick = () => {
+    switcher = !switcher;
+    renderTask();
+}
+
+function correctDate(dueDate) {
+    if (dueDate) {
+        const day = new Date(dueDate).getDate();
+        const noZeroMonth = new Date(dueDate).getMonth() + 1;
+        const month = noZeroMonth.toString().length === 1 ? (0 + noZeroMonth.toString()) : noZeroMonth;
+        const year = new Date(dueDate).getFullYear();
+        return `${day}.${month}.${year}`;
+    } else return '~no date~';
+}
+
+function renderTask(){
+    tasks.innerHTML = '';
+    data.forEach(generateTasks);
+}
 
 function overdue(dueDate) {
     const now = new Date();
-    const day = now.getDate();
-    const month = now.getMonth() + 1;
-    const year = now.getFullYear();
-    const fullDate = `${day}.${month}.${year}`;
-    console.log(fullDate);
-    return now < dueDate ? true : false;
+    now.setHours(0, 0, 0, 0);
+    return now > dueDate;
 }
-console.log(overdue(new Date('2022-08-25')));
 
+function deleteTask(event) {
+    const target = event.target.parentElement.parentElement;
+    target.remove();
+    const deleteParentId = target.id;
+    const task = data.find(t => t.id === +deleteParentId);
+    data.splice(data.indexOf(task), 1);
+}
 
-class isCheck {
-    constructor(check) {
-        if (check.checked) {
-            this.done = check.checked;
-        } else {
-            this.done = check.checked;
-        }
+function doneSwitcher(event) {
+    const target = event.target.parentElement;
+    const checkboxParentId = event.target.parentElement.parentElement.parentElement.id;
+    const task = data.find(t => t.id === +checkboxParentId);
+    if (task.done) {
+        task.done = !task.done;
+        target.style.textDecoration = "none";
+    } else {
+        task.done = !task.done;
+        target.style.textDecoration = "line-through";
+    }
+    renderTask();
+}
+
+let data = [
+    {id: 1, dueDate: new Date('2022-08-27'), done: false, name: 'hometask', description: null},
+    {id: 2, dueDate: new Date('2022-08-28'), done: false, name: 'officetask', description: 'make office work'},
+    {id: 3, dueDate: new Date('2022-08-27'), done: true, name: 'shopping', description: 'buy vegetables'},
+    {id: 4, dueDate: null, done: true, name: 'walk', description: 'go for a walk'},
+    {id: 5, dueDate: new Date('2022-08-26'), done: false, name: 'CSS Figma', description: 'to do adaptive mobile first'}
+];
+
+function generateTasks(fields) {
+    const {dueDate, done, name, description} = fields;
+    const textDecoration = done ? 'line-through' : 'none';
+    const dateColor = (overdue(dueDate) && !done) ? 'red' : '';
+    tasks.innerHTML += `
+    <div id="${fields.id}" class="tasksElement">
+        <div class="date" style="color: ${dateColor}">
+        ${correctDate(dueDate)}
+        </div>
+        <div class="doneNameDesc">
+    <label style="text-decoration:${textDecoration}"><input onclick="doneSwitcher(event)" type="checkbox" name="checkfield" id="isDone" ${done ? 'checked' : ''}>
+    <strong>${name}</strong></label>
+    </div>
+    <p class="desc">${description ? description : '~no description~'} <br>
+    </p>
+    <span class="delete">
+    <button onclick="deleteTask(event)">delete</button>
+    </span>
+    <hr>
+    </div>`
+    if (!switcher){
+    hideTasks(fields.id, done);
+    } else {
+        hideTasks(fields.id, false);
     }
 }
-
-const generateTasks = (id, dueDate, done, name, description) => {
-    const day = new Date(dueDate).getDate();
-    const month = new Date(dueDate).getMonth() + 1;
-    const year = new Date(dueDate).getFullYear();
-    const fullDate = `${day}.${month}.${year}`
-    return `
-    <div class="todo-item">
-        <div class="date">
-        ${fullDate}
-        </div>
-        <div class="checkAndName">
-    <label><input type="checkbox" name="checkfield" id="isDone" ${done ? 'checked' : ''}><strong>${name}</strong></label>
-    </div>
-    <p class="desc">${description}<hr></p>
-    </div>`
+function hideTasks(id, done) {
+    const tasksElement = document.getElementById(id);
+    tasksElement.classList.toggle('hiddenTask', done);
 }
-
-const taskHTML = data.map(task => {
-    return generateTasks(task.id, task.dueDate, task.done, task.name, task.description);
-}).join('');
-
-tasks.innerHTML = taskHTML;
-
-console.log(tasks);
+renderTask();
